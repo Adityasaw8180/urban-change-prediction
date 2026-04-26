0045fc0c626d7c36a9f8f361df34aae626c944fd
Adityasaw8180 <https://github.com/Adityasaw8180>
2026-04-26 23:39:07 +0530

    Setup Node.js server and routes

--- backend-node/routes/predict.js
+++ backend-node/routes/predict.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const router = express.Router();
-
+const router  = express.Router();
 const { predictUrban } = require('../controllers/predictController');
 
 router.post('/predict', predictUrban);
